9daa306b3a1b73fe0652812844a4fc5961734bd3
pythonman1234 <107050272+pythonman1234@users.noreply.github.com>
2023-10-06 20:59:21 +0530

    Add files via upload

---
+++ Day-1/continue.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Hi" << endl;
+        cout << "Hey" << endl;
+        continue;
+
+        cout << "Message kar" << endl;
+
+        return 0;
+    }
+}
