bd1741f5c8597eab1dce5c2cbe8e5b531279f5dc
Mann Shah <107050272+pythonman1234@users.noreply.github.com>
2023-11-14 05:33:37 +0530

    Add files via upload

---
+++ Day-40/singlyLinkedList.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    // constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+    // destructor
+    ~Node()
+    {
+        int value = this->data;
+        // memory free
+        if (this->next != NULL)
+        {
+            delete next;
+            this->next = NULL;
+        }
+        cout << " memory is free for node with data " << value << endl;
+    }
+};
+
+void insertAtHead(Node *&head, int d)
+{
+    Node *temp = new Node(d);
+    temp->next = head;
+    head = temp;
+}
+
+void insertAtTail(Node *&tail, int d)
+{
+    Node *temp = new Node(d);
+    tail->next = temp;
+    tail = temp;
+}
+
+void print(Node *head)
+{
+    if (head == NULL)
+    {
+        cout << "List is empty" << endl;
+        return;
+    }
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->data << "";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void insertAtPosition(Node *tail, Node *&head, int position, int d)
+{
+    // insert at start
+    if (position == 1)
+    {
+        insertAtHead(head, d);
+        return;
+    }
+
+    Node *temp = head;
+    int cnt = 1;
+
+    while (cnt < position - 1)
+    {
+        temp = temp->next;
+        cnt++;
+    }
+
+    // inserting at last position
+    if (temp->next == NULL)
+    {
+        insertAtTail(tail, d);
+        return;
+    }
+
+    // creating a node for d
+    Node *nodeToInsert = new Node(d);
+    nodeToInsert->next = temp->next;
+
+    temp->next = nodeToInsert;
+}
